9d0b19a3636772e5018c7bf121da25232a561b8d
Sushir Sourav Nanda <sushirsouravnanda@gmail.com>
2024-11-10 09:32:44 +0530

    initial commit

--- src/main/java/com/soft/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/soft/eureka_server/EurekaServerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
+		system.out.println("hii");
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
